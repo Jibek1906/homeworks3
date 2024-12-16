888140877a5b2f4ba1d611f718ec803571f18561
Jibek1906 <jibek.cabirova@mail.ru>
2024-12-16 10:50:14 +0600

    homework 4

--- handlers/review_dialog.py
+++ handlers/review_dialog.py
@@ -10,21 +10,29 @@ class RestaurantReview(StatesGroup):
     food_rating = State()
     cleanliness_rating = State()
     extra_comments = State()
+    date = State()
 
 @review_dialog_router.callback_query(F.data == "review")
-async def review(callback: types.CallbackQuery, state: FSMContext):
+async def start_process(callback: types.CallbackQuery, state: FSMContext):
     await callback.answer()
     await callback.message.answer("Как вас зовут?")
     await state.set_state(RestaurantReview.name)
 
 @review_dialog_router.message(RestaurantReview.name)
-async def review2(message: types.Message, state: FSMContext):
+async def process_name(message: types.Message, state: FSMContext):
+    name = message.text
+    if not name.isalpha():
+        await message.answer("Пишите имя буквами")
+        return
+    if len(name) < 3 or len(name) > 15:
+        await message.answer("Количество символов должно быть от 3 до 15")
+        return
     await state.update_data(name=message.text)
     await message.answer("Ваш номер телефона")
     await state.set_state(RestaurantReview.phone_number)
 
 @review_dialog_router.message(RestaurantReview.phone_number)
-async def review3(message: types.Message, state: FSMContext):
+async def process_phone_number(message: types.Message, state: FSMContext):
     phone_number = message.text
     if not phone_number.isdigit():
         await message.answer("Пожалуйста, вводите только цифры")
@@ -50,6 +58,15 @@ async def review3(message: types.Message, state: FSMContext):
 @review_dialog_router.callback_query(RestaurantReview.food_rating)
 async def process_food_rating(callback: types.CallbackQuery, state: FSMContext):
     if callback.data in ['1', '2', '3', '4', '5']:
+        rating = int(callback.data)
+        if rating >= 1 and rating <= 3:
+            await callback.message.answer(
+                f"Вы поставили {callback.data}. Спасибо за честный отзыв, мы постараемся улучшить нашу кухню.")
+        elif rating >= 4 and rating <= 5:
+            await callback.message.answer(
+                f"Вы поставили {callback.data}. Спасибо за положительный отзыв. Будем стараться удерживать данный уровень кухни.")
+        await state.update_data(food_rating=callback.data)
+
         await state.update_data(food_rating=callback.data)
         cleanliness_rating_kb = types.InlineKeyboardMarkup(
             inline_keyboard=[
@@ -77,13 +94,30 @@ async def process_food_rating(callback: types.CallbackQuery, state: FSMContext):
 @review_dialog_router.callback_query(RestaurantReview.cleanliness_rating)
 async def process_cleanliness_rating(callback: types.CallbackQuery, state: FSMContext):
     if callback.data in ['bad', 'not_good', 'good', 'perfect', 'amazing']:
+        food_rating = callback.data
+        if food_rating in ['bad', 'not_good']:
+            await callback.message.answer("Сожалеем, что вам не понравилась чистота нашего заведения.")
+        elif food_rating == 'good':
+            await callback.message.answer("Будем стараться сделать наше заведение чище.")
+        elif food_rating in ['perfect', 'amazing']:
+            await callback.message.answer("Очень рады, что вам понравилась чистота нашего заведения.")
         await state.update_data(cleanliness_rating=callback.data)
         await callback.message.answer("Дополнительные комментарии/жалоба")
         await state.set_state(RestaurantReview.extra_comments)
 
 @review_dialog_router.message(RestaurantReview.extra_comments)
-async def review6(message: types.Message, state: FSMContext):
+async def process_extra_comments(message: types.Message,state: FSMContext):
+    extra_comments = message.text
+    if len(extra_comments) > 500:
+        await message.answer("Ваши текст слишком длинный. Пожалуйста, ограничьтесь 500 символами.")
+        return
     await state.update_data(extra_comments=message.text)
+    await message.answer("Дата посещения?")
+    await state.set_state(RestaurantReview.date)
+
+@review_dialog_router.message(RestaurantReview.date)
+async def process_date(message: types.Message, state: FSMContext):
+    await state.update_data(date=message.text)
     await message.answer(f"Спасибо за ваш отзыв, {message.from_user.first_name}")
     data = await state.get_data()
     print(data)
